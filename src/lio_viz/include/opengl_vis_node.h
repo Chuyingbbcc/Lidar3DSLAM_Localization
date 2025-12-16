baa3a670c7894bf908535d90519d6148664e9417
Chuyingbbcc <clu348@gatech.edu>
2025-12-15 17:50:59 -0800

    change the ptr rendering compatible with opengl core 3.3

--- src/lio_viz/include/opengl_vis_node.h
+++ src/lio_viz/include/opengl_vis_node.h
@@ -5,7 +5,13 @@
 
 #include <rclcpp/rclcpp.hpp>
 #include <std_msgs/msg/string.hpp>
+#include <glad.h>
 #include <GLFW/glfw3.h>
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
 
 #include <vector>
 #include <string>
@@ -14,7 +20,7 @@ using Point3f =Point<float,3>;
 class OpenGLPointCloudNode : public rclcpp::Node {
 public:
     OpenGLPointCloudNode();
-    ~OpenGLPointCloudNode() = default;
+    ~OpenGLPointCloudNode() override;
 
     bool init_window();
     void render_frame(float t);
@@ -23,6 +29,8 @@ public:
 private:
     void on_pcd_path(const std_msgs::msg::String::SharedPtr msg);
     bool load_point_cloud(const std::string &path, std::vector<Point3f> &out_points);
+    bool init_gl_resources();
+    void upload_points_to_gpu(const std::vector<Point3f> &out_points);
 
     rclcpp::Subscription<std_msgs::msg::String>::SharedPtr sub_;
     std::vector<Point3f> points_;
@@ -30,6 +38,13 @@ private:
     std::mutex mutex_;
 
     GLFWwindow* window_{nullptr};
+
+
+   GLuint vao_;
+   GLuint vbo_;
+   GLuint shader_program_;
+   bool vbo_ready_;
+   std::size_t num_points_gpu_;
 
    // event call back
    float zoom_;
